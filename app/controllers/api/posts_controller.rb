b73bae88138f392c2eabb88dc2f6e23d85012c55
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-04-08 16:30:17 -0700

    controlerrs almost finished

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,51 @@
 class Api::PostsController < ApplicationController
+  
+    #defines the CRUD actions before using them
+    before_action :set_post, only: [:show, :destroy, :update]
+
+    #finds all Posts in DB
+    def index 
+        render json: Post.all
+    end
+     
+    #finds specific Post in DB 
+    def show 
+        render json: @post
+    end
+    
+    #creates a Post in DB
+    def create
+        post = Post.new(post_params)
+        if(post.save)
+          render json: post
+        else
+          render json: {errors: post.errors.full_messages}, status: 422
+        end
+    end
+  
+     #updates a Post in DB (edit)
+     def update
+        if(@post.update(post_params))
+          render json: @post
+        else
+          render json: {errors: @post.errors.full_messages}, status: 422
+       end
+    end
+  
+    #deletes a Post in DB
+    def destroy
+        render json: @post.destroy
+    end
+  
+    private
+    #function that allows @user to find specific user
+    def set_post
+        @post = Post.find(params[:id])
+    end
+  
+    #function for users that requires a name to create a user for DB
+    #must require a username to work
+    def post_params
+        params.require(:post).permit(:title, :meme, :user_id)
+    end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,50 @@
 class Api::UsersController < ApplicationController
+  #defines the CRUD actions before using them
+  before_action :set_user, only: [:show, :destroy, :update]
+
+  #finds all Users in DB
+  def index 
+      render json: User.all
+  end
+   
+  #finds specific User in DB
+  def show 
+      render json: @user
+  end
+  
+  #creates a User in DB
+  def create
+      user = User.new(user_params)
+      if(user.save)
+        render json: user
+      else
+        render json: {errors: user.errors.full_messages}, status: 422
+      end
+  end
+
+   #updates a User in DB (edit)
+   def update
+      if(@user.update(user_params))
+        render json: @user
+      else
+        render json: {errors: @user.errors.full_messages}, status: 422
+     end
+  end
+
+  #deletes a User in DB
+  def destroy
+      render json: @user.destroy
+  end
+
+  private
+  #function that allows @user to find specific user
+  def set_user
+      @user = User.find(params[:id])
+  end
+
+  #function for users that requires a name to create a user for DB
+  #must require a username to work
+  def user_params
+      params.require(:user).permit(:name, :email, :password)
+  end
 end
